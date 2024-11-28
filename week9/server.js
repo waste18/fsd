6aa3638aa234903f2e49855707b425a613fe2c23
waste18 <minnunidhi85@gmail.com>
2024-11-28 19:01:05 +0530

    Create server.js

---
+++ week9/server.js
@@ -0,0 +1,9 @@
+ const http = require('http');
+ const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello, this is a custom server!');
+ });
+ const PORT = 3000;
+ server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+ });
